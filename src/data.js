32675625ab99c2df56449e7a93d8f48a41a00e27
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2022-08-24 12:10:32 +0530

    Example data, Synchronize with local strorage

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ let data=[
         id:1,
         name:"Jai Singh",
         age:29,
+        date:"2014-02-09",
         day:"26 Apr",
         image:"https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
@@ -10,6 +11,7 @@ let data=[
         id:2,
         name:"Ishita",
         age:91,
+        date:"2014-02-09",
         day:"26 Apr",
         image:"https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
@@ -17,6 +19,7 @@ let data=[
         id:3,
         name:"Raju",
         age:45,
+        date:"2014-02-09",
         day:"23 Mar",
         image:"https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     }
